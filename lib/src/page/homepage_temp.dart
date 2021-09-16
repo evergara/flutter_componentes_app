f3b11010fe7bf066e6c7aa2704834a511d6d5875
emersonvergara <emerson.vergara@ceiba.com.co>
2021-09-16 18:50:04 -0500

    creamos un ListView dinamicos de la forma tradicionar y de forma corta

--- lib/src/page/homepage_temp.dart
+++ lib/src/page/homepage_temp.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
-
+  final verbos = ["Be", 'Have', 'Pay', 'See', 'Speak', 'Take','keep','Live', 'Life'];
+  static const String VERBS_ING = 'ing';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,21 +10,42 @@ class HomePageTemp extends StatelessWidget {
           title: Text('Component App'),
         ),
         body: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text('Lista de registros'),
-            ),
-            Divider(),
-            ListTile(
-              title: Text('Lista de registros'),
-            ),
-            Divider(),
-            ListTile(
-              title: Text('Lista de registros'),
-            ),
-            Divider()
-          ],
+          children: _crearItemsWidgetCota()
         )
     );
+  }
+
+  List<Widget> _crearItemsWidget(){
+    List<Widget> widgets = [];
+    for(String item in verbos ){
+      final widgetTemp = ListTile(
+        title: Text(item),
+      );
+      /*
+      *Esto lo remplaza el operador de cascada
+      widgets.add(widgetTemp);
+      widgets.add(Divider(height: 30.0));
+      */
+      //Uso del operador de cascada
+      widgets..add(widgetTemp)
+             ..add(Divider(height: 20.0));
+    }
+   return widgets;
+  }
+  List<Widget> _crearItemsWidgetCota(){
+     return verbos.map((item){
+       return Column(
+         children: [
+           ListTile(
+             title: Text(item),
+             subtitle:Text('$item$VERBS_ING') ,
+             leading: Icon(Icons.account_balance_wallet),
+             trailing: Icon(Icons.keyboard_arrow_right),
+             onTap: (){},
+           ),
+           Divider(height: 15.0)
+         ],
+       );
+     }).toList();
   }
 }
